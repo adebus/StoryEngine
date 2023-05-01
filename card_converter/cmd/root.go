894a52af6dcedac704740cb15fbaca695b1dda14
Adam Debus <adam@debus.net>
2023-04-30 23:14:50 -0700

    Read file, count the number of each type of card

--- card_converter/cmd/root.go
+++ card_converter/cmd/root.go
@@ -1,14 +1,48 @@
 /*
 Copyright © 2023 Adam Debus
-
 */
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type cardAgent struct {
+	side1 string
+	side2 string
+	side3 string
+	side4 string
+}
+
+type cardEngine struct {
+	side1 string
+	side2 string
+}
+
+type cardAnchor struct {
+	side1 string
+	side2 string
+	side3 string
+	side4 string
+}
+
+type cardConflict struct {
+	side1 string
+	side2 string
+}
+
+type cardAspect struct {
+	side1 string
+	side2 string
+	side3 string
+	side4 string
+}
 
 var (
 	// Input file
@@ -37,8 +71,51 @@ func Execute() {
 	}
 }
 
-func rootRun(cmd *cobra.Command, args []string) {
+func checkErr(e error, msg string) {
+	if e != nil {
+		log.Println(msg+": "+e.Error())
+		os.Exit(1)
+	}
+}
 
+func rootRun(cmd *cobra.Command, args []string) {
+	inFile, err := os.Open(inputFile)
+	checkErr(err, "Unable to open the input file")
+	defer inFile.Close()
+
+	scanner := bufio.NewScanner(inFile)
+	
+	var (
+		agentCount int = 0
+		engineCount int = 0
+		anchorCount int = 0
+		conflictCount int = 0
+		aspectCount int = 0
+	)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(),",")
+		switch cardType := line[1]; cardType {
+		case "Agent":
+			agentCount++
+		case "Engine":
+			engineCount++
+		case "Anchor":
+			anchorCount++
+		case "Conflict":
+			conflictCount++
+		case "Aspect":
+			aspectCount++
+		default:
+			fmt.Println("Line doesn't have a card type: ", cardType)
+		}
+	}
+
+	fmt.Println("Number of Agents: ", agentCount)
+	fmt.Println("Number of Engines: ", engineCount)
+	fmt.Println("Number of Anchors: ", anchorCount)
+	fmt.Println("Number of Conflicts: ", conflictCount)
+	fmt.Println("Number of Aspects: ", aspectCount)
 }
 
 func init() {
@@ -57,4 +134,32 @@ func init() {
 	rootCmd.MarkFlagRequired("outputdir")
 }
 
+// Create a new instance of the cardAgent struct
+func newAgent(s1 string, s2 string, s3 string, s4 string) *cardAgent {
+	c := cardAgent{side1: s1, side2: s2, side3: s3, side4: s4}
+	return &c
+}
 
+// Create a new instance of the cardEngine struct
+func newEngine(s1 string, s2 string) *cardEngine {
+	c := cardEngine{side1: s1, side2: s2}
+	return &c
+}
+
+// Create a new instance of the cardAnchor struct
+func newAnchor(s1 string, s2 string, s3 string, s4 string) *cardAnchor {
+	c := cardAnchor{side1: s1, side2: s2, side3: s3, side4: s4}
+	return &c
+}
+
+// Create a new instance of the cardConflict struct
+func newConflict(s1 string, s2 string) *cardConflict {
+	c := cardConflict{side1: s1, side2: s2}
+	return &c
+}
+
+// Create a new instance of the cardAspect struct
+func newAspect(s1 string, s2 string, s3 string, s4 string) *cardAspect {
+	c := cardAspect{side1: s1, side2: s2, side3: s3, side4: s4}
+	return &c
+}
